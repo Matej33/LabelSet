074c136d5891b0184ed901a096d69703216232c8
Matej Púčik <matopucik@gmail.com>
2026-03-29 23:45:16 +0200

    implement Dijkstra's algorithm for shortest path calculation and update .gitignore

--- main.cpp
+++ main.cpp
@@ -1,18 +1,100 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <limits>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+using namespace std;
 
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
+long INFINITY = numeric_limits<long>::max();
 
-    const auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+struct Edge {
+    long u, v, c;
+};
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+void LabelSetAlgo(long u, long v, long numOfVertices, const vector<Edge>& allEdges) {
+    vector<pair<long, long>> t_x(numOfVertices, {INFINITY, 0});
+
+    vector<vector<pair<long, long>>> neighbors(numOfVertices + 1);
+    for (auto& edge : allEdges) {
+        neighbors[edge.u].emplace_back(edge.v, edge.c);
     }
 
+    priority_queue<pair<long, long>, vector<pair<long, long>>, greater<>> epsilons;
+
+    t_x[u - 1].first = 0;
+    epsilons.emplace(0, u);
+
+    while (!epsilons.empty()) {
+        long tr = epsilons.top().first;
+        long r = epsilons.top().second;
+        epsilons.pop();
+
+        if (tr > t_x[r - 1].first) continue;
+
+        if (r == v) break;
+
+        for (const auto& n : neighbors[r]) {
+            long j = n.first;
+            long c = n.second;
+
+            long newDist = t_x[r - 1].first + c;
+
+            if (newDist < t_x[j - 1].first) {
+                t_x[j - 1].first = newDist;
+                t_x[j - 1].second = r;
+                epsilons.emplace(newDist, j);
+            }
+        }
+    }
+
+    if (t_x[v - 1].first == INFINITY) {
+        cout << "Path does not exist." << endl;
+    } else {
+        cout << "Shortest distance: " << t_x[v - 1].first << endl;
+
+        vector<long> finalPath;
+        for (long i = v; i != 0; i = t_x[i - 1].second) {
+            finalPath.push_back(i);
+        }
+        reverse(finalPath.begin(), finalPath.end());
+
+        cout << "Path:";
+        for (long i : finalPath) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    string fileName = "../input.hrn";
+    ifstream file(fileName);
+
+    if (!file.is_open()) {
+        cerr << "Couldn't open file: " << fileName << '\n';
+        return 1;
+    }
+
+    vector<Edge> allEdges;
+    long a, b, c;
+    long biggestVertex = 0;
+
+    while (file >> a >> b >> c) {
+        if (max(a, b) > biggestVertex) biggestVertex = max(a, b);
+        allEdges.push_back({a, b, c});
+    }
+    file.close();
+
+    long start, end;
+    cout << "Enter start vertex: ";
+    cin >> start;
+    cout << "Enter end vertex: ";
+    cin >> end;
+
+    LabelSetAlgo(start, end, biggestVertex, allEdges);
+
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
